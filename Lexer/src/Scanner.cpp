304a2cb8f0c27c292d2d523fb32a48f042bb8a77
Spencer Brown <spencerb@warpmail.net>
2018-02-23 14:59:12 -0500

    Add Lexer sub-component and Scanner class.

---
+++ Lexer/src/Scanner.cpp
@@ -0,0 +1,5 @@
+//
+// Created by spencer on 2/23/18.
+//
+
+#include "Scanner.h"
---
+++ Lexer/src/Scanner.h
@@ -0,0 +1,13 @@
+//
+// Created by spencer on 2/23/18.
+//
+
+#ifndef COMPILER8080_SCANNER_H
+#define COMPILER8080_SCANNER_H
+
+
+class Scanner {
+};
+
+
+#endif //COMPILER8080_SCANNER_H
