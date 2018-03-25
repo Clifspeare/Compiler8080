e3d08139d3a0fd7dda93a1e8823731afd6a2af01
Spencer Brown <spencerb@warpmail.net>
2018-03-25 17:07:03 -0400

    Change some file structure stuff and move the "main" routine to a test executable. Also, hello_stuff.h and hello_world.c are now in resources where they should be, since they're not actual compilable/executable programs (at least for our purposes).

--- tests/testlexingroutine.cpp
+++ tests/testlexingroutine.cpp
@@ -9,9 +9,13 @@ int main(int argc, char *argv[]) {
     char filepath[]="hello_world.c";
 
     bool doRawLog = false;
+    bool print = false;
     if (argc > 1) {
         if (strcmp(argv[1], "-log") == 0) {
             doRawLog = true;
+        }
+        if (strcmp(argv[1], "-print") == 0) {
+            print = true;
         }
     }
     Scanner lexer(filepath, doRawLog);
@@ -20,6 +24,8 @@ int main(int argc, char *argv[]) {
     int counter = 0;
     token = preprocessor.getNextToken();
     while (token.value != "end") {
+        if (print)
+            std::cout << token.value << std::endl;
         token = preprocessor.getNextToken();
     }
     std::cout << "end reached" << std::endl;
